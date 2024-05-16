099c61da1f183ba7d7b48d2f619552b233c3fbdb
vittor <vittor.pelicarto137@gmail.com>
2024-05-16 13:28:12 -0300

    Generating password hash

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,5 +1,6 @@
 import { Body, ConflictException, Controller, HttpCode, Post } from '@nestjs/common'
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'src/prisma/prisma.service'
+import { hash } from 'bcryptjs'
 
 @Controller()
 export class CreateAccountController {
@@ -20,11 +21,13 @@ export class CreateAccountController {
       throw new ConflictException('User with same email already exists')
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
       }
     })
   }
